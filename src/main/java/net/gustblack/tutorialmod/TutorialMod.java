90ef44db57bd72781918f7b31c5aef313490995f
Bl4ck <110741409+TheBl4ckG4mer@users.noreply.github.com>
2024-08-04 17:25:09 -0300

    Initial commit

---
+++ src/main/java/net/gustblack/tutorialmod/TutorialMod.java
@@ -0,0 +1,20 @@
+package net.gustblack.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.gustblack.tutorialmod.item.ModItemGroups;
+import net.gustblack.tutorialmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+
+	}
+}
